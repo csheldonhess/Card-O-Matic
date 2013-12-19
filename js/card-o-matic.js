6577d6b1aebf6f96e79f98411e062af2871abb4d
Coral Sheldon-Hess <coral@sheldon-hess.org>
2013-12-18 22:36:18 -0900

    Now it has a greeting and a recipient.

--- js/card-o-matic.js
+++ js/card-o-matic.js
@@ -13,3 +13,27 @@ $(".textures").click(function(){
 	// and make the card the right texture
 	$("#canvas").css("background-image", thetexture);
 }); 
+
+$(".messages").click(function() {
+	// who got clicked?
+	var theradio = $(this);
+	// ok, and which label does that go with?
+	var thelabel = theradio.next();
+	// what does the label say? (ring ding ding ding dingeding)
+	var thetext = thelabel.html();
+	// set the text up on the card
+	$("#message-output").html(thetext);
+});
+
+$("#recipient").keyup(function() {
+	// what name did they type in?
+	var thename = $(this).val();
+	$("#recipient-output").html(thename);
+	// is it too long?
+	if (thename.length >= 14) {
+		$("#text-error").html(" Max characters: 14");
+	}
+	else {
+		$("text-error").html("");
+	}
+});
